ce663ffddd4b1bc0e40afa47ff26fd52cb5f4847
qaza3 <tomdale55@hotmail.com>
2026-02-02 11:28:13 +0000

    u

--- setup.sh
+++ setup.sh
@@ -7,19 +7,32 @@
 # Ensure scripts are executable
 find . -name '*.bash' -exec chmod 700 {} \;
 
+clear
+
 for i in {01..17}; do
+    idx=${i#0}
     # 1. Removed space from "Q$i *" to match "Q$i-Name"
     # 2. Quoted $target to handle potential spaces in folder names
     target=$(find ./a/ -maxdepth 1 -type d -name "Q$i*" | head -n 1)
     
     if [ -n "$target" ]; then
         # Map aliases directly to the discovered path
-        alias q$i="clear; cat \"$target/Questions.bash\""
-        alias s$i="\"$target/LabSetUp.bash\""
-        alias sol$i="\"$target/SolutionNotes.bash\""
+        alias q$idx="clear; cat \"$target/Questions.bash\""
+        alias s$idx="\"$target/LabSetUp.bash\""
+        alias sol$idx="\"$target/SolutionNotes.bash\""
         
         echo "Aliases created for $target"
     else
         echo "Warning: Q$i not found in ./a/"
     fi
 done
+
+alias ql='. ~/a/setup.sh'
+
+echo '--'
+echo '-- q1   - show qestion 1'
+echo '-- s1   - setup question 1'
+echo '-- sol1 - show solution question 1'
+echo '-- -- replace "1" with the question number you want'
+echo '-- ql   - show question list'
+echo '--'
