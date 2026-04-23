02a14e86045919d2838cbe08c18a35dc5ed0b74f
qaza3 <tomdale55@hotmail.com>
2026-04-23 13:21:57 +0100

    Update SolutionNotes.bash

--- Q07-PriorityClass/SolutionNotes.bash
+++ Q07-PriorityClass/SolutionNotes.bash
@@ -5,3 +5,38 @@ kubectl get pc
 # Patch deployment to use it
 kubectl patch deployment busybox-logger -n priority -p '{"spec":{"template":{"spec":{"priorityClassName":"high-priority"}}}}'
 kubectl describe deployment busybox-logger -n priority | grep -i "Priority Class"
+
+or
+
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: high-priority
+value: 999
+globalDefault: false
+
+k get deployment busybox-logger -n priority -o yaml > q7_dep.yaml
+
+add in line 
+...
+template:
+        metadata:
+          labels:
+            app: busybox-logger
+        spec:
+          containers:
+          - command:
+            - sh
+            - -c
+            - while true; do echo 'logging...'; sleep 5; done
+            image: busybox
+            imagePullPolicy: Always
+            name: busybox
+            resources: {}
+            terminationMessagePath: /dev/termination-log
+            terminationMessagePolicy: File
+          dnsPolicy: ClusterFirst
+          priorityClassName: high-priority
+          restartPolicy: Always
+          schedulerName: default-scheduler
+...
