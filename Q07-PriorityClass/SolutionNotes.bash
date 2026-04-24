84d401070f9e675928bf373e7e1f7f86ada42ec0
qaza3 <tomdale55@hotmail.com>
2026-04-24 08:22:26 +0100

    Update SolutionNotes.bash

--- Q07-PriorityClass/SolutionNotes.bash
+++ Q07-PriorityClass/SolutionNotes.bash
@@ -1,6 +1,24 @@
+#show current
+
+k get pc
+
+NAME                      VALUE        GLOBAL-DEFAULT   AGE     PREEMPTIONPOLICY
+system-cluster-critical   2000000000   false            22d     PreemptLowerPriority
+system-node-critical      2000001000   false            22d     PreemptLowerPriority
+user-critical             1000         false            2m18s   PreemptLowerPriority
+
 # Create PriorityClass just below existing max (e.g., 999)
 kubectl create priorityclass high-priority --value=999 --description="high priority"
 kubectl get pc
+
+# or
+
+apiVersion: scheduling.k8s.io/v1
+kind: PriorityClass
+metadata:
+  name: high-priority
+value: 999
+globalDefault: false
 
 # Patch deployment to use it
 kubectl patch deployment busybox-logger -n priority -p '{"spec":{"template":{"spec":{"priorityClassName":"high-priority"}}}}'
@@ -40,3 +58,5 @@ template:
           restartPolicy: Always
           schedulerName: default-scheduler
 ...
+
+
